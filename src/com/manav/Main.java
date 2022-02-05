7a854040d6b44f30e1ae008a4b7cd59360465ea0
manav <globalgroup850@gmail.com>
2022-02-05 19:35:11 +0530

    Initial Changes

--- src/com/manav/Main.java
+++ src/com/manav/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 //        Change the program to allow players to type full words, or phrases, then move to the
-//        coorect location based upon their input.
+//        correct location based upon their input.
 //        The player should be able to type commands such as "Go West", "run South", or just "East"
 //        and the program will move to the appropriate location if there is one. As at present, an
 //        attempt to move in an invalid direction should print a message and remain in the same place.
